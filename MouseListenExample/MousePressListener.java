d8a55ef2894a225b6cee95eefc96eaf9a17171e3
jhperales <jhperales@stu.naperville203.org>
2016-02-12 16:40:10 -0600

    Revert "Revert "more things""
    This reverts commit 8700f1eb09d9c30b2e2e7590bf965d59555c607e.

---
+++ MouseListenExample/MousePressListener.java
@@ -0,0 +1,28 @@
+import java.awt.event.*;
+
+public class MousePressListener implements MouseListener
+{
+    public void mousePressed(MouseEvent event)
+    {
+        int x = event.getX();
+        int y = event.getY();
+        RectangleComponent2 component = new RectangleComponent2();
+        component.moveRectangleTo(x, y);
+    }
+    // Do-nothing methods
+    public void mouseReleased(MouseEvent event)
+    {
+    }
+
+    public void mouseClicked(MouseEvent event) 
+    {
+    }
+
+    public void mouseEntered(MouseEvent event) 
+    {
+    }
+
+    public void mouseExited(MouseEvent event) 
+    {
+    }
+}
---
+++ TriangleGUI/MousePressListener.java
@@ -0,0 +1,26 @@
+import java.awt.event.*;
+
+public class MousePressListener implements MouseListener
+{
+    public void mousePressed(MouseEvent event)
+    {
+        
+    }
+    // Do-nothing methods
+    public void mouseReleased(MouseEvent event)
+    {
+    }
+
+    public void mouseClicked(MouseEvent event) 
+    {
+    }
+
+    public void mouseEntered(MouseEvent event) 
+    {
+    }
+
+    public void mouseExited(MouseEvent event) 
+    {
+    }
+}
+    
