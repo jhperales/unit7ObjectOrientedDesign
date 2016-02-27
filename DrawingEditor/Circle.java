bf9fb4acaf694f3f86d4c951fe556b9cad4c5862
jhperales <jhperales@stu.naperville203.org>
2016-02-27 14:27:57 -0600

    am i doing this correctly? probably not :P

--- DrawingEditor/Circle.java
+++ DrawingEditor/Circle.java
@@ -1,3 +1,13 @@
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
 import java.awt.Graphics2D;
+import java.awt.Color;
 
+public class Circle extends Shape
+{
+    center = 50.0;
+    radius = 20.0;
+    color = new Color(0, 0, 0);
+    
+}
+    
