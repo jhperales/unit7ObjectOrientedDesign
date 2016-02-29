854f1a842eb16c1f9b2c204e347fe2b078e8b943
jhperales <jhperales@NN123-HA-10.HS.sd203.org>
2016-02-29 14:12:24 -0600

    eh

--- DrawingEditor/Square.java
+++ DrawingEditor/Square.java
@@ -1,19 +1,28 @@
 import java.awt.geom.*;
 import java.awt.*;
 
-
 public class Square extends Shape
 {
-    
+
     public Square(Point2D.Double p, double r, Color c)
     {
         super(p, r, c);
     }
-    
+
     public void draw(Graphics2D g2, boolean filled)
     {
         Rectangle2D.Double rec = new Rectangle2D.Double(getRadius() *2, getRadius() * 2, getCenter().getX(), getCenter().getY());
+        if (filled == true)
+        {
+            g2.draw(rec);
+            g2.fill(rec);
+        }
+        else
+        {
+            g2.draw(rec);
+        }
     }
+
     
     public boolean isInside(Point2D.Double point)
     {
